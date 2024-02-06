564bac381a057510e30ea3bf68349db1dcfb485c
tarikulabir <tarikulabir931@gmail.com>
2024-02-06 21:14:46 +0600

    Home

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
-class home extends StatelessWidget {
+class home extends StatefulWidget {
   const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  bool light = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
+      appBar: AppBar(
+        title: ListTile(
+          leading: Icon(Icons.home),
+        ),
+        actions: [
+          Icon(Icons.notifications),
+          Switch.adaptive(
+            applyCupertinoTheme: false,
+            value: light,
+            onChanged: (bool value) {},
+          )
+        ],
+      ),
     );
   }
 }
