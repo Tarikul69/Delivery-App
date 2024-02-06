be217a254760411cf10fe2b46bddadd4e1d3119e
tarikulabir <tarikulabir931@gmail.com>
2024-02-06 21:23:48 +0600

    Theme Change

---
+++ lib/components/theme/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+ThemeData lighiMood = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.white,
+    primary: Colors.grey,
+    secondary: Colors.purple,
+  ),
+);
+ThemeData darkMood = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    background: Colors.black,
+    primary: Colors.grey,
+    secondary: Colors.orange,
+  ),
+);
