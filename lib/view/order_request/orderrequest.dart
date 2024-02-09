f56cc9166585f41da0d54923fa5d697ff0665ce7
tarikulabir <tarikulabir931@gmail.com>
2024-02-09 18:47:19 +0600

    Update

--- lib/view/order_request/orderrequest.dart
+++ lib/view/order_request/orderrequest.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class orderrequest extends StatelessWidget {
+class orderrequest extends StatefulWidget {
   const orderrequest({super.key});
 
+  @override
+  State<orderrequest> createState() => _orderrequestState();
+}
+
+class _orderrequestState extends State<orderrequest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +20,85 @@ class orderrequest extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+              mycard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget mycard() {
+    return Card(
+      elevation: 5,
+      color: Theme.of(context).colorScheme.secondary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: SizedBox(
+        width: Get.width,
+        height: 175,
+        child: Column(
+          children: [
+            ListTile(
+              leading: CircleAvatar(),
+              title: Text("Tarikul Abir"),
+              subtitle: Text("bangladesh"),
+              trailing: TextButton(onPressed: () {}, child: Text("data")),
+            ),
+            Text(
+              "3 Items",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "2 Minutes ago",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(Get.width / 3, 40),
+                      shadowColor: Theme.of(context).colorScheme.primary,
+                    ),
+                    onPressed: () {},
+                    child: Text("Ignore"),
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(Get.width / 3, 40),
+                      shadowColor: Theme.of(context).colorScheme.secondary,
+                    ),
+                    onPressed: () {},
+                    child: Text("Accept"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
