f3b64f8b23db13e8e81cfebfd87d306f506919ce
tarikulabir <tarikulabir931@gmail.com>
2024-02-23 18:42:43 +0600

    Update

--- lib/view/notification/notification.dart
+++ lib/view/notification/notification.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class notification extends StatelessWidget {
   const notification({super.key});
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Notification"),
+        title: Text(
+          "Notification",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [notificationCard()],
+        ),
+      ),
+    );
+  }
+
+  Widget notificationCard() {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text(
+              "Notification",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text("Notification details"),
+          ),
+          Text(
+            DateFormat('kk:mm:ss  EEE d MMM').format(DateTime.now()),
+            style: TextStyle(),
+          ),
+          Divider()
+        ],
+      ),
     );
   }
 }
